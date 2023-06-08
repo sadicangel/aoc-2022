852e8ce788323b320a345d4f980f80762bfe99c7
sadicangel <sadicangel@hotmail.com>
2023-06-08 23:57:39 +0100

    Adds solution for part 2.

--- zig/01/src/main.zig
+++ zig/01/src/main.zig
@@ -8,23 +8,21 @@ pub fn main() !void {
     var buf: [1024]u8 = undefined;
     var max: u64 = 0;
     var current: u64 = 0;
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    var elves = std.ArrayList(u64).init(gpa.allocator());
     while (try in_stream.readUntilDelimiterOrEof(&buf, '\n')) |line| {
         if (line.len == 0) {
-            std.debug.print("{}\n", .{current});
+            try elves.append(current);
+            // std.debug.print("{}\n", .{current});
             max = std.math.max(max, current);
             current = 0;
         } else {
-            std.debug.print("- {s}\n", .{line});
+            // std.debug.print("- {s}\n", .{line});
             const num = try std.fmt.parseUnsigned(u64, line, 10);
             current += num;
         }
     }
-    std.debug.print("{}\n", .{max});
-}
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+    std.debug.print("Max Calories Top 1: {}\n", .{max});
+    std.sort.sort(u64, elves.items, {}, std.sort.desc(u64));
+    std.debug.print("Max Calories Top 3: {}\n", .{elves.items[0] + elves.items[1] + elves.items[2]});
 }
