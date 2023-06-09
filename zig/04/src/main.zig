21005e19cb4d7ea22eca5950f009b470ec9797f3
sadicangel <sadicangel@hotmail.com>
2023-06-09 12:16:27 +0100

    Adds solution for part 1.

---
+++ zig/04/src/main.zig
@@ -0,0 +1,25 @@
+const std = @import("std");
+
+pub fn main() !void {
+    const content = @embedFile("input.txt");
+    var readIter = std.mem.tokenize(u8, content, "\n");
+    var inside: u32 = 0;
+    while (readIter.next()) |line| {
+        var rangeIter = std.mem.split(u8, line, ",");
+        const range1 = rangeIter.next().?;
+        const range2 = rangeIter.next().?;
+        var splitIter1 = std.mem.split(u8, range1, "-");
+        var splitIter2 = std.mem.split(u8, range2, "-");
+        const min1 = try std.fmt.parseUnsigned(i32, splitIter1.next().?, 10);
+        const max1 = try std.fmt.parseUnsigned(i32, splitIter1.next().?, 10);
+        const min2 = try std.fmt.parseUnsigned(i32, splitIter2.next().?, 10);
+        const max2 = try std.fmt.parseUnsigned(i32, splitIter2.next().?, 10);
+        const r1WithinR2 = min1 >= min2 and max1 <= max2;
+        const r2WithinR1 = min2 >= min1 and max2 <= max1;
+        if (r1WithinR2 or r2WithinR1) {
+            inside += 1;
+            //std.debug.print("{}-{},{}-{}\n", .{ min1, max1, min2, max2 });
+        }
+    }
+    std.debug.print("Ranges inside other range: {}\n", .{inside});
+}
