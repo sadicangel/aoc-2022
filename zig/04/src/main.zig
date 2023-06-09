9c5d9b52e3df01b7b5fddf9bc5182452320bbac3
sadicangel <sadicangel@hotmail.com>
2023-06-09 14:14:45 +0100

    Adds solution for part 2.

--- zig/04/src/main.zig
+++ zig/04/src/main.zig
@@ -4,6 +4,7 @@ pub fn main() !void {
     const content = @embedFile("input.txt");
     var readIter = std.mem.tokenize(u8, content, "\n");
     var inside: u32 = 0;
+    var overlap: u32 = 0;
     while (readIter.next()) |line| {
         var rangeIter = std.mem.split(u8, line, ",");
         const range1 = rangeIter.next().?;
@@ -14,12 +15,14 @@ pub fn main() !void {
         const max1 = try std.fmt.parseUnsigned(i32, splitIter1.next().?, 10);
         const min2 = try std.fmt.parseUnsigned(i32, splitIter2.next().?, 10);
         const max2 = try std.fmt.parseUnsigned(i32, splitIter2.next().?, 10);
+
         const r1WithinR2 = min1 >= min2 and max1 <= max2;
         const r2WithinR1 = min2 >= min1 and max2 <= max1;
-        if (r1WithinR2 or r2WithinR1) {
+        if (r1WithinR2 or r2WithinR1)
             inside += 1;
-            //std.debug.print("{}-{},{}-{}\n", .{ min1, max1, min2, max2 });
-        }
+        if (min1 <= max2 and min2 <= max1)
+            overlap += 1;
     }
-    std.debug.print("Ranges inside other range: {}\n", .{inside});
+    std.debug.print("Ranges inside other: {}\n", .{inside});
+    std.debug.print("Ranges that overlap: {}\n", .{overlap});
 }
