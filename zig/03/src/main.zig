559e218267ae94a586c2f161cfdbb8ad4d24379b
sadicangel <sadicangel@hotmail.com>
2023-06-09 10:43:20 +0100

    Adds solution to part 2.

--- zig/03/src/main.zig
+++ zig/03/src/main.zig
@@ -1,6 +1,10 @@
 const std = @import("std");
 
-pub fn main() !void {
+fn getPriority(c: u8) u32 {
+    return if (c >= 'a') c - 'a' + 1 else c - 'A' + 27;
+}
+
+fn part1() !void {
     const content = @embedFile("input.txt");
     var readIter = std.mem.tokenize(u8, content, "\n");
     var priority: u32 = 0;
@@ -15,9 +19,46 @@ pub fn main() !void {
                 }
             }
         }
-        const p = if (c >= 'a') c - 'a' + 1 else c - 'A' + 27;
-        std.debug.print("{c}: {}\n", .{ c, p });
+        const p = getPriority(c);
+        //std.debug.print("{c}: {}\n", .{ c, p });
         priority += p;
     }
-    std.debug.print("{}\n", .{priority});
+    std.debug.print("Individual priorities: {}\n", .{priority});
+}
+
+fn part2() !void {
+    const content = @embedFile("input.txt");
+    var readIter = std.mem.tokenize(u8, content, "\n");
+    var priority: u32 = 0;
+    while (readIter.next()) |line1| {
+        const line2 = readIter.next();
+        const line3 = readIter.next();
+        //std.debug.print("{?s}\n{?s}\n{?s}\n", .{ line1, line2, line3 });
+        var c: u8 = undefined;
+        var found = false;
+        for (line1) |c1| {
+            if (found)
+                break;
+            for (line2.?) |c2| {
+                if (found)
+                    break;
+                if (c1 != c2)
+                    continue;
+                for (line3.?) |c3| {
+                    if (c1 == c3) {
+                        c = c1;
+                        break;
+                    }
+                }
+            }
+        }
+        //std.debug.print("{c}\n\n", .{c});
+        priority += getPriority(c);
+    }
+    std.debug.print("Group priorities: {}\n", .{priority});
+}
+
+pub fn main() !void {
+    try part1();
+    try part2();
 }
